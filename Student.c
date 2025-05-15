2c3f19c2595f7d107fbc0d8c696edc741e986a6f
chethantm722 <cts406542@gmail.com>
2025-05-15 09:12:35 +0530

    Create Student.c

---
+++ Student.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int i, j, k=1,marks[3][3];
+printf ("enter the marks of students :"
+for (1=0;1<3; 1++) for (j=0; j<3;j++)
+scanf ("%d", & marks[i][j]):
+for (i=0;i<3; i++)
+printf ("\n marks of students %d is:"
+, k
+):
+for (J=0;j<3;]++)
+printf("d", marks [i][j]) ;
+k=k+1;
+}
+return 0;
+}
