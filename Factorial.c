b85b5a9341dd204a19898387218387c55f601734
chethantm722 <cts406542@gmail.com>
2025-05-15 09:10:22 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,12 @@
+▶
+#include<stdio.h>
+int main){
+int n,arr[n], sum =0,1;
+printf("enter the arry elements
+scanf ("%d", &n) ;
+printf("enter the elements\n");
+for (1=0;1<n; 1++){
+scanf ("%d",&arr [n]);
+sum +=arr[n];
+printf("%d\n"
+, sum) ;
