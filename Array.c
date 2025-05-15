1fc8fba2e22abf480a31aeaf0a3b123138efc3f4
chethantm722 <cts406542@gmail.com>
2025-05-15 09:14:12 +0530

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main)
+int arr[10],i, sum=0;
+printf ("enter 10 elements to
+array");
+for (i=0;i<10;i++)
+{
+scanf ("%d"
+, &arr [1]):
+sum=sum+arr [1];
+}
+printf ("sum of all array elements
+i5%d", sum) ;
+return 0;
+}
